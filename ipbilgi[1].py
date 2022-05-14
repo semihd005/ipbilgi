207988d942659c67c3c8707f8494570e03c37237
BYdoktor <82740545+semihd005@users.noreply.github.com>
2022-05-14 22:27:23 +0300

    Add files via upload

---
+++ ipbilgi[1].py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+#tool yerine kendi toolunuzun ismini yazın.
+os.system("figlet İpSorgu")
+
+print("""
+Seonly Hizmetinizde!
+Hos Geldiniz. Islemler:
+
+1- Hizli Tarama
+2- Servis ve Versiyon Bilgisi
+
+""")
+
+no = input("Bir Islem Seciniz: ")
+
+if(no == "1"): #eğer no = 1 ise asagidaki islemleri yap.
+    ip = input("Hedef IP: ") #ip değerini girilecek ip adresine esitliyoruz.
+    os.system("nmap " + ip) #terminalden nmap'de calisacak komutu yaziyoruz.
+elif(no == "2"):
+    ip = input("Hedef IP: ")
+    os.system("nmap -sS -sV " + ip)
+else:
+    print("Hatali Secim.")
